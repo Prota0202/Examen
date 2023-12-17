94edc264d86aae01bac4ed1e9b641a04f952fa66
Abdelbadi Lababidi <Abdelbadi5@protonmail.com>
2023-12-17 18:25:25 +0100

    commit

---
+++ LW3L-orm/models/Task.js
@@ -0,0 +1,8 @@
+import Model from './Model.js';
+
+export default class Task extends Model {
+
+  static table = "users.Television";
+  static primary = ["id"];
+}
+
---
+++ LW3L-orm/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import Task from './models/Task.js';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/add", async function (req, res) {
+  const task = new Task();
+  task.Brand = req.body.Brand;
+  task.Price = req.body.Price;
+  task.Size = req.body.Size;
+  task.Bought = 0;
+  task.Cause = req.body.Cause;
+  await task.save();
+  res.redirect('/');
+});
+
+app.get("/", async function (req, res) {
+  const brokenTasks = await Task.loadMany({Bought : 1 , Broken : 1})
+  const wishlistTasks = await Task.loadMany({Bought : 0});
+  const tasks = await Task.loadMany({Bought : 1 });
+  res.render('listTasks.ejs', { wishlistTasks, tasks , brokenTasks });
+});
+
+
+app.get("/delete/:id", async function (req, res) {
+  await Task.delete({ id: req.params.id });
+  res.redirect('/');
+});
+
+
+app.get('/broken/:id', async function(req, res){
+  const task = await Task.load({id: req.params.id});
+  task.Broken = 1;
+  task.Bought = 1;
+  res.render("broken.ejs", {task : task});
+});
+
+app.get("/buy/:id", async function (req, res) {
+  const task = await Task.load({ id: req.params.id });
+  task.Bought = 1;
+  task.Broken = 0;
+  await task.save();
+  res.redirect('/')
+});
+
+app.use(express.static('public'));
+
+app.get('/moveToWishlist/:id', async (req, res) => {
+  const id = req.params.id;
+  const task = await Task.load({ id: id });
+  task.Bought = 0;
+  await task.save();
+  res.redirect('/'); // Rediriger vers la page principale
+});
+
+
+app.listen(4000);
